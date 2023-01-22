5f5e29b2f5ae51b3c8eec39144800f494f578179
AleLep <lepkowska.ola@gmail.com>
2023-01-22 06:10:05 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by aleksandralepkowska on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
